de4193d62464283f4e455d71493f55796c371f5c
odemyanenko <olgadem@hotmail.com>
2019-09-23 11:43:46 +0300

    change properties for postgres db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO TBL_USERS (first_name, last_name, email) VALUES
+INSERT INTO USERS (first_name, last_name, email) VALUES
   ('Vasya', 'Pupkin', 'vpupkin@gmail.com'),
   ('Dasha', 'Ivanova', 'divanova@gmail.com'),
   ('Ivan', 'Petrov', 'petrov@gmail.com');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS TBL_USERS;
+DROP TABLE IF EXISTS USERS;
 
-CREATE TABLE TBL_USERS (
+CREATE TABLE USERS (
                                id INT AUTO_INCREMENT  PRIMARY KEY,
                                first_name VARCHAR(250) NOT NULL,
                                last_name VARCHAR(250) NOT NULL,
